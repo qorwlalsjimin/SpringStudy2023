419d463ae0d067a12f84a5324bdd6f62e4c31466
Baek Jimin <qorwlalsjimin@naver.com>
2023-03-07 16:06:39 +0900

    강한 결합과 약한 결합

--- src/Strong_Weak_DI.java
+++ src/Strong_Weak_DI.java
@@ -2,24 +2,25 @@ import java.util.Date;
 
 public class Strong_Weak_DI {
     public static void main(String[] args) {
-        Date date = new Date();
+        Date date = new Date(); //강한 결합
         System.out.println("date1:"+date);
+
     }
     public static void getDate(Date d){
-        Date d2 = d;
+        Date d2 = d; //약한 결합
         System.out.println("date2:"+d2);
     }
-    
+
     public static void MemberUse1(){
-        Member m1 = new Member();
+        Member m1 = new Member(); //강한 결합
     }
     public static void MemberUse2(Member m){
-        Member m2 = m;
+        Member m2 = m; //약한 결합 //느슨하다
     }
 }
 
 class Member{
     String name;
     String nickName;
-    public Member(){};
+    private Member(){}; //생성자를 private으로하면 강한 결합에서만 오류 발생
 }
